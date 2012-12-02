079a0b76eff8077ff89390e6372218932065d44c
Tom White <tom@sixdozen.com>
2012-12-01 18:30:29 -0800

    clocky-d3: Cleaned up debug code.

--- clocky-d3/public/d3/d3.patches.js
+++ clocky-d3/public/d3/d3.patches.js
@@ -5,15 +5,8 @@
         // if a filter has been installed and it should be called...
         if(d3.selection.prototype.hasOwnProperty("dataToArray") &&
                 arguments.length > 0) {
-            //console.log("Fixing stray : " + typeof arguments[0]);
-            //console.log(typeof d3.selection.prototype.dataToArray);
             arguments[0] = d3.selection.prototype.dataToArray(arguments[0]);
         }
-        // console.log("Filter installed");
-        // console.log(arguments);
         return orig_data.apply(this, Array.prototype.slice.call(arguments));
     };
 }());
-
-                // typeof arguments[0] !== "function" &&
-                // Object.prototype.toString.call( someVar ) !== '[object Array]') {
