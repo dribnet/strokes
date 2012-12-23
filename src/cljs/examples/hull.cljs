c94f99d38e27e5ebb15975a20ed7fb874fa6f703
Tom White <tom@sixdozen.com>
2012-12-22 19:55:42 -0800

    hull: vertices now stored as vector in atom

--- src/cljs/examples/hull.cljs
+++ src/cljs/examples/hull.cljs
@@ -8,16 +8,29 @@
 ; RandThought: Why can't .random return .-random? (autopopulate)
 (def randomX (-> d3 .-random (.normal (/ width 2) 60)))
 (def randomY (-> d3 .-random (.normal (/ height 2) 60)))
-(def vertices
-  (apply array 
+
+; this holds on to the mutable state of the vertex array
+(def vert-atom
+  (atom
     (for [x (range 100)]
-      (apply array [(randomX) (randomY)]))))
+      [(randomX) (randomY)])))
+
+; drefs and converts the vector of vector to an array of arrays for d3 consumption
+(defn vert-array []
+  (apply array (map #(apply array %) @vert-atom)))
+
+; forward declaration of redraw function, which sadly can't be called 'redraw'
+(def redrawhull)
 
 (def svg
   (-> d3 (.select "body") (.append "svg")
     (.attr "width" width)
-    (.attr "height" height)))
+    (.attr "height" height)
+;    (.on "click" #(.log js/console (.mouse d3 js/this)))
+    (.on "click" #(do (swap! vert-atom conj [100 100]) (redrawhull)))
+    ))
 
+; draw a border
 (-> svg (.append "rect")
     (.attr "width" width)
     (.attr "height" height))
@@ -30,21 +43,18 @@
   (atom (.selectAll svg "circle")))
 
 (defn redrawhull[]
-  (-> (.datum hull 
-    (-> d3 .-geom (.hull vertices)))
-      (.attr "d" #(str "M" (join "L" %) "Z")))
-  (swap! circle #(.data % vertices))
-  ;(swap! circle .data (apply array vertices))
-  ;(swap! circle .data vertices)
-  ;(swap! circle #(.data %))
-  ;(-> d3 (.selectAll "circle") (.data vertices))
-  (-> @circle .enter (.append "circle")
-    (.attr "r" 3))
-  (-> @circle
-    (.attr "transform" #(str "translate(" % ")"))))
+  (let [verts (vert-array)]
+    (-> (.datum hull 
+      (-> d3 .-geom (.hull verts)))
+        (.attr "d" #(str "M" (join "L" %) "Z")))
+    (swap! circle #(.data % verts))
+    (-> @circle .enter (.append "circle")
+      (.attr "r" 3))
+    (-> @circle
+      (.attr "transform" #(str "translate(" % ")")))))
 
 (defn ^:export launch []
   (redrawhull)
-  (.log js/console (apply array vertices))
+  (.log js/console (vert-array))
   ; (redrawhull)
   true)
