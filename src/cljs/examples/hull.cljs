d6658553f7c9e955e78982ceef25aba0dba80a7d
Tom White <tom@sixdozen.com>
2012-12-23 23:18:33 -0800

    hull: example cleaned up and localized to launch function

--- src/cljs/examples/hull.cljs
+++ src/cljs/examples/hull.cljs
@@ -4,64 +4,48 @@
 
 (def width 960)
 (def height 500)
+(def centerpoint [(/ width 2) (/ height 2)])
 
 ; RandThought: Why can't .random return .-random? (autopopulate)
+
+; two functions that return random normal distributions
 (def randomX (-> d3 .-random (.normal (/ width 2) 60)))
 (def randomY (-> d3 .-random (.normal (/ height 2) 60)))
 
-; this holds on to the mutable state of the vertex array
+; this holds on to the (global) mutable state of the vertex array
 (def vert-atom
   (atom
-    (for [x (range 5)]
-      [(randomX) (randomY)])))
+    ; init to 20 random points with the centerpoint at the end
+    (conj
+      (for [x (range 20)]
+        [(randomX) (randomY)])
+      centerpoint)))
 
-; drefs and converts the vector of vector to an array of arrays for d3 consumption
+; derefs and converts the vector of vector to an array of arrays for d3 consumption
 (defn vert-array []
   (apply array (map #(apply array %) @vert-atom)))
 
-; forward declaration of redraw function, which sadly can't be called 'redraw'
-(def redrawhull)
-
-(def svg
+; create root svg element
+(defn gen-svg []
   (-> d3 (.select "body") (.append "svg")
     (.attr "width" width)
-    (.attr "height" height)
-    ; with mousemove, replace last element with mouse position
-    (.on "mousemove" (fn [] 
-      (this-as t (let [pt (js->clj (.mouse d3 t))]
-        (swap! vert-atom #(conj (rest %) pt))
-        (redrawhull)))))
-    ; with click, replace last element with mouse posistion x2
-    (.on "click" (fn [] 
-      (this-as t (let [pt (js->clj (.mouse d3 t))]
-        (swap! vert-atom #(conj (rest %) pt pt))
-        (redrawhull)))))
-    ;?(.on "mouseexit" #(.log js/console "mouseexit happened"))
-    ;?(.on "mouseenter" #(.log js/console "mouseenter happened"))
-    ;(.on "mouseexit" (fn [] (swap! vert-atom #(conj (rest %) [(/ width 2) (/ height 2)]))))
-    ;(.on "mousewheel" #(.log js/console "mousewheel happened"))
-    ;(.on "mouseup" #(.log js/console "mouseup happened"))
-    ;(.on "click" #(.log js/console "click happened"))
-    ;(.on "dblclick" #(.log js/console "dblclick happened"))
-    ;(.on "mouseover" #(.log js/console "mouseover happened"))
-  ))
+    (.attr "height" height)))
 
 ; draw a border
-(-> svg (.append "rect")
-    (.attr "width" width)
-    (.attr "height" height))
+(defn draw-border [svg]
+  (-> svg (.append "rect")
+      (.attr "width" width)
+      (.attr "height" height)))
 
-(def hull
+; create hull element
+(defn gen-hull [svg]
   (-> svg (.append "path")
       (.attr "class" "hull")))
 
-(def circle 
-  (atom (.selectAll svg "circle")))
-
-(defn redrawhull[]
+; redraw hull and points. called after any changes to vert-atom
+(defn redraw-hull[hull circle]
   (let [verts (vert-array)]
-    (-> (.datum hull 
-      (-> d3 .-geom (.hull verts)))
+    (-> (.datum hull (-> d3 .-geom (.hull verts)))
         (.attr "d" #(str "M" (join "L" %) "Z")))
     (swap! circle #(.data % verts))
     (-> @circle .enter (.append "circle")
@@ -69,8 +53,30 @@
     (-> @circle
       (.attr "transform" #(str "translate(" % ")")))))
 
+; memo: http://en.wikipedia.org/wiki/DOM_events
+(defn add-mouse-callbacks [svg hull circle]
+  (-> svg
+    ; with mousemove, replace last element with mouse position
+    (.on "mousemove" (fn [] 
+      (this-as t (let [pt (js->clj (.mouse d3 t))]
+        (swap! vert-atom #(conj (rest %) pt))
+        (redraw-hull hull circle)))))
+    ; with click, replace last element with mouse posistion x2
+    (.on "click" (fn [] 
+      (this-as t (let [pt (js->clj (.mouse d3 t))]
+        (swap! vert-atom #(conj (rest %) pt pt))
+        (redraw-hull hull circle)))))
+    ; when exiting, replace last element with centerpoint
+    (.on "mouseout" (fn [] 
+      (swap! vert-atom #(conj (rest %) centerpoint))
+      (redraw-hull hull circle)))
+  ))
+
 (defn ^:export launch []
-  (redrawhull)
   ;(.log js/console (vert-array))
-  ; (redrawhull)
-  true)
+  (let [svg (gen-svg)
+        hull (gen-hull svg)
+        circle (atom (.selectAll svg "circle"))]
+    (draw-border svg)
+    (add-mouse-callbacks svg hull circle)
+    (redraw-hull hull circle)))
