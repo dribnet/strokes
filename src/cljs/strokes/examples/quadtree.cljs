de78d544f331b8b34ebcc48f05aecc6c48bacea1
Tom White <tom@sixdozen.com>
2012-12-29 12:09:59 -0800

    quadtree example: code cleanup for blocks preparation

--- src/cljs/strokes/examples/quadtree.cljs
+++ src/cljs/strokes/examples/quadtree.cljs
@@ -17,26 +17,6 @@
   (for [x (range 2500)]
     (patch-map {:x (rand width), :y (rand height)})))
 
-; this works, but is crazy-slow
-; (defn nodes [quadtree]
-;   (let [sidecar (atom [])]
-;     (.visit quadtree (fn [n, x1, y1, x2, y2]
-;       (swap! sidecar conj {:x x1, :y y1, :width (- x2 x1), :height (- y2 y1)})
-;       ; return false to keep processing...
-;       false))
-;     ; return the accumulated side effects
-;     @sidecar))
-
-; this works, but feels dirty
-; (defn nodes [quadtree]
-;   (let [sidecar (array)]
-;     (.visit quadtree (fn [n, x1, y1, x2, y2]
-;       (.push sidecar {:x x1, :y y1, :width (- x2 x1), :height (- y2 y1)})
-;       ; return false to keep processing...
-;       false))
-;     ; return the accumulated side effects
-;     (seq sidecar)))
-
 ; gets a seq of all nodes. (the quadtree.visit api seems to beg for this side effecty impl)
 (defn nodes [quadtree]
   (let [sidecar (transient [])]
@@ -47,57 +27,41 @@
     ; return the accumulated side effects
     (persistent! sidecar)))
 
-
 ; sadly, the quadtree and point must be global for the brush callback to work
 (def quadtree (atom nil))
 (def point (atom nil))
 
-; forward declaration of to be defined brushed callback
-; -- sadly, this didn't work so I forward declared the brush instead
-; (declare brushed)
-
 ; forward declaration of brush
 (def brush)
 
+; determine intersection of brush and nodes, color using css classes
 (defn brushed []
-  (.log js/console "brushed")
   (let [extent (.extent brush)
         scanned (transient #{})
         selected (transient #{})
-        qt @quadtree
-        pt @point
         x0 (nth (nth extent 0) 0)
         y0 (nth (nth extent 0) 1)
         x3 (nth (nth extent 1) 0)
         y3 (nth (nth extent 1) 1)]
 
-    (.visit qt (fn [n, x1, y1, x2, y2]
+    ; visit nodes and add nodes to sets
+    (.visit @quadtree (fn [n, x1, y1, x2, y2]
       (let [p (.-point n)
             px (if p (.-x p) nil)
             py (if p (.-y p) nil)]
         ; mark as scanned and maybe selected
         (if p (do
-          ;(.log js/console (str "scanned " p))
           (conj! scanned p)
           (if (and (>= px x0) (< px x3) (>= py y0) (< py y3))
             (conj! selected p))))        
         ; return value determines if we keep traversing
         (or (>= x1 x3) (>= y1 y3) (< x2 x0) (< y2 y0)))))
 
-    ; now do something with what we know
-    (.classed pt "scanned" #(scanned %))
-    (.classed pt "selected" #(selected %))))
+    ; now update the view based on what we learned
+    (.classed @point "scanned" #(scanned %))
+    (.classed @point "selected" #(selected %))))
 
-    ; (.visit quadtree (fn [n, x1, y1, x2, y2]
-    ;   (let [p (.point n)]
-    ;     ; mark as scanned and maybe selected
-    ;     (if p (do
-    ;       (conj! scanned n)
-    ;       (if (and (>= (.x p) )))
-    ;       ))
-
-    ; )))))
-
+; on screen brush widget
 (def brush 
   (-> d3 .-svg (.brush)
     (.x (-> d3 .-scale (.identity) (.domain [0, width])))
@@ -136,5 +100,4 @@
         (.attr "class" "brush")
         (.call brush))
 
-    (brushed)
-))
+    (brushed)))
--- src/cljs/strokes/mrhyde.cljs
+++ src/cljs/strokes/mrhyde.cljs
@@ -3,34 +3,25 @@
   (:use [clojure.string :only [join]]
         [cljs.reader :only [read-string]]))
 
-; todo: let's make a method that hooks into patched-array constructors
-; (defn check-headroom [this proto & args]
-;   (.log js/console (str " :type is: " (type proto)))
-;   )
-
 ; a replacement for the map call
-; thanks Arthur Ulfeldt: http://stackoverflow.com/questions/1651351/clojure-call-a-function-for-each-element-in-a-vector-with-it-index
 (defn mapish [f]
   (this-as ct
     (doall (map 
       #(.call f js/undefined % %2 ct) (seq ct) (iterate inc 0)))))
 
 ; a replacement for the foreach call (which is map that returns null)
-; note - eachish is supposed to be able to mutate the original array,
-; but that shouldn't matter in the persistent world we are creating
 (defn eachish [f]
   ; call mapish with the same 'this'
   (this-as ct (.call mapish ct f))
   nil)
 
+; Add functionality to cljs seq prototype to make it more like a js array
 (defn patch-prototype-as-array [p]
   ; array length call
   (.__defineGetter__ p "length" #(this-as t (count t)))
   ; access by index... we obviously need a smarter upper bound here
   (dotimes [n 5000] 
     (.__defineGetter__ p n #(this-as t (nth t n js/undefined))))
-  ; (-> p .-bareConstructor (set! (-> p .-constructor)))
-  ; (-> p .-constructor (set! (fn [& args] (this-as t (check-headroom t p args)))))
   ; if we are acting like an array, we'll need in impl of forEach and map
   (-> p .-forEach (set! eachish))
   (-> p .-map (set! mapish))
