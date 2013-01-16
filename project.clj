dbe0d148318f915bfeae68a6e87fa11ec5f7268b
Tom White <tom@sixdozen.com>
2013-01-16 00:09:16 -0800

    strokes: project changes for packaging jar

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject strokes "0.0.1-SNAPSHOT"
+(defproject net.drib/strokes "0.0.1-SNAPSHOT"
   :description "strokes: d3 from clojurescript"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :min-lein-version "2.0.0"
-  :source-paths ["src/clj"]
+  :source-paths ["src/clj" "src/cljs"]
 
   :plugins [[lein-cljsbuild "0.2.10"]]
 
