7dda7173713c8eaca35b5a3625944fe412020830
Tom White <tom@sixdozen.com>
2013-02-17 03:24:15 -0800

    release: strokes-0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.drib/strokes "0.3.1-SNAPSHOT"
+(defproject net.drib/strokes "0.3.1"
   :description "strokes: d3 from clojurescript"
   :url "https://github.com/dribnet/strokes"
   :license {:name "Eclipse Public License"
