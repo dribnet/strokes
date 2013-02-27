48dc5fb158e641d913a294e536215ade25c3c4e0
Tom White <tom@sixdozen.com>
2013-02-27 03:52:11 -0800

    zoom: in progress
    App mostly working, and setting up zoom code.

--- zip-decode/src/cljs/zip-decode.cljs
+++ zip-decode/src/cljs/zip-decode.cljs
@@ -8,6 +8,13 @@
 (def width 1200)
 (def height 600)
 
+(def svg (-> d3 (.select "#map") (.append "svg")
+    (.attr {:width width
+            :height height})))
+
+(def draw-root (-> svg (.append "g")))
+(def debug-rect (-> draw-root (.append "rect") (.attr "class" "debug")))
+
 ; debug helper
 (defn dp [& args]
   (.log js/console (apply str args)) )
@@ -22,8 +29,59 @@
 
 (def zipdots-sel (atom nil))
 
+(defn reset-zoom []
+ (-> draw-root (.transition) (.duration 1500) 
+          (.attr "transform" ""))) 
+
+(defn re-zoom []
+  (let [dots (-> draw-root (.selectAll "text.selected"))
+        lats   (array)
+        lons   (array)]
+
+    (-> dots (.each (fn [d]
+      ; (.log js/console d)
+        (.push lats (aget d "lat"))
+        (.push lons (aget d "lon"))
+      )))
+
+    (if-let [ct (count lats)]
+      (let [min-lat (reduce min lats)
+            max-lat (reduce max lats)
+            min-lon (reduce min lons)
+            max-lon (reduce max lons)
+            [x1 y1] (proj-fn [min-lon min-lat])
+            [x2 y2] (proj-fn [max-lon max-lat])
+            [xmin xmax] (if (< x1 x2) [x1 x2] [x2 x1])
+            [ymin ymax] (if (< y1 y2) [y1 y2] [y2 y1])]
+
+        (dp "bounds: " min-lat "," min-lon " - " max-lat "," max-lon)
+        (dp "proj: " xmin "," ymin " - " xmax "," ymax)
+
+        ; (-> debug-rect (.attr {
+        ;   :x xmin
+        ;   :y ymin
+        ;   :width (- xmax xmin)
+        ;   :height (- ymax ymin)
+        ;   }))
+        ; ; TODO: zoom!
+        ; (-> draw-root (.transition) (.duration 1500) 
+        ;   (.attr "transform"
+        ;     (str "translate(" (.translate proj-fn) ")"
+        ;       "scale(" (/ 0.95 (max (/ (- xmax xmin) width)
+        ;                             (/ (- ymax ymin) height))) ")"
+        ;       "translate(" (/ (+ xmax xmin) 2) ","
+        ;                    (/ (+ ymax ymin) 2) ")")
+        ;     ))
+      ))
+
+    ; return true to stop
+    true))
+
 (defn update-selection [s]
   (let [len (count s)]
+    (if (> len 0)
+      (-> d3 (.timer re-zoom))
+      (-> d3 (.timer reset-zoom)))
     (-> @zipdots-sel (.attr "class" (fn [d]
       (let [zip (aget d "zip")
             sub (.substr zip 0 len)]
@@ -31,28 +89,37 @@
           "selected"
           "unselected")))))))
 
+; for another day - why is this slow and broken?
+; (defn update-selection [s]
+;   (let [len (count s)]
+;     (-> @zipdots-sel (.each (fn [d]
+;       (this-as t
+;         (let [zip (aget d "zip")
+;               sub (.substr zip 0 len)
+;               hit (and (> len 0) (= s sub))]
+;           (-> d3 (.select t) (.attr "class"
+;             (if hit)
+;               "selected"
+;               "unselected")))))))))
+
 (defn key-fn []
   (if @zipdots-sel
     (update-selection (-> input-node (.text)))))
 
 (-> input-node (.on "keyup" key-fn))
 
-(def svg (-> d3 (.select "#map") (.append "svg")
-    (.attr {:width width
-            :height height})))
-
 (defn first-render [maproot ziproot]
   ; clear "loading" text
   (-> status-node (.remove))
 
-  (-> svg (.append "g") (.attr "id" "states") (.selectAll "path")
+  (-> draw-root (.append "g") (.attr "id" "states") (.selectAll "path")
       (.data (aget maproot "features"))
     (.enter)
       (.append "path")
       (.attr "d" path-fn))
 
   (reset! zipdots-sel
-    (-> svg (.append "g") (.attr "id" "zipdots") (.selectAll "text")
+    (-> draw-root (.append "g") (.attr "id" "zipdots") (.selectAll "text")
         (.data ziproot)
       (.enter)
         (.append "text")
