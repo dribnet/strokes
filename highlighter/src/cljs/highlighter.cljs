e3c04841e6c426e2fc3d5456c3f615ae199e9c1f
Tom White <tom@sixdozen.com>
2013-03-10 00:26:35 -0800

    highlighter: can now show/hide sections as well
    not the most intuitive key bindings, but that's ok for now.

--- highlighter/src/cljs/highlighter.cljs
+++ highlighter/src/cljs/highlighter.cljs
@@ -10,18 +10,27 @@
 (def all-classes
   (join "," (map #(str ".high" %) v10)))
 
+; q w e r t y u i o p
+(def hide-keys [81 81 87 69 82 84 89 85 73 79 80])
+(def hide-keys-indices (zipmap hide-keys (range)))
+
 ; returns decision from keys
 ; {:all-off true/false, turn-on: [1,2,...]}
 (defn key-decision [code shift]
   ; where is clojure.core.char? oh well, sorry.
-  (if (< 48 code 59)
-    {:all-off shift :turn-on [(- code 48)]}
-    (case [code shift]
+  (cond 
+    (< 48 code 59)
+      {:all-off shift :turn-on [(- code 48)]}
+    (contains? hide-keys-indices code)
+      (if shift
+        {:visible [(get hide-keys-indices code)]}
+        {:invisible [(get hide-keys-indices code)]})
+    :else (case [code shift]
       ; map '0' to '10'
       [48 false]  {:all-off false :turn-on [10]}
       [48 true]   {:all-off true  :turn-on [10]}
-      [192 false] {:all-off true  :turn-on []}
-      [192 true]  {:all-off false :turn-on v10}
+      [192 true]  {:all-off true}
+      [192 false] {:all-off false :turn-on v10 :visible v10}
       (do 
         ; -> debug show unknown keys... (.log js/console code) 
         nil))))
@@ -31,8 +40,15 @@
       (-> d3 .-event .-shiftKey))]
     (if (:all-off d)
       (-> d3 (.selectAll all-classes)
-        (.classed "highlight" false)))
+        (.classed "highlight" false)
+        (.classed "invisible" false)))
     (doseq [n (:turn-on d)]
       (-> d3 (.selectAll (str ".high" n))
         (.classed "highlight" true))) 
+    (doseq [n (:invisible d)]
+      (-> d3 (.selectAll (str ".high" n))
+        (.classed "invisible" true))) 
+    (doseq [n (:visible d)]
+      (-> d3 (.selectAll (str ".high" n))
+        (.classed "invisible" false))) 
 ))))
