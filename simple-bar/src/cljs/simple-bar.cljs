64d2880efb52981ffa5f4013b6dac2217b70cee0
Tom White <tom@sixdozen.com>
2013-03-15 07:40:51 -0700

    simple-bar: now with strokes-less version

--- simple-bar/src/cljs/simple-bar.cljs
+++ simple-bar/src/cljs/simple-bar.cljs
@@ -1,16 +1,61 @@
 (ns simple-bar
   (:require [strokes :refer [d3]]))
 
-(strokes/bootstrap)
-
 ; rect: data ↦ width, index ↦ y
 ; adapted from mike bostocks slide presentation
+
+;;;; shared data across the 3 examples
 
 (def data [63 39 31 53 25 32 175 69 51])
 
 (def m [50 40 50 40])
 (def w (- 960 (m 1) (m 3)))
 (def h (- 500  (m 0) (m 2)))
+
+;;;; a world without strokes (to compare code)
+
+; x is a fn: data ↦ width
+(def x1 (-> d3 .-scale (.linear)
+  (.domain (array 0 (apply max data)))
+  (.range (array 0 w))))
+
+; y is a fn: index ↦ y
+(def y1 (-> d3 .-scale (.ordinal)
+  (.domain (apply array (range (count data))))
+  (.rangeRoundBands (array 0 h) 0.2)))
+
+(def svg1 (-> d3 (.select "#static") (.append "svg")
+    (.attr (clj->js {:width  (+ w (m 1) (m 3))
+            :height (+ h (m 0) (m 2))}))
+  (.append "g")
+    (.attr (clj->js {:transform (str "translate(" (m 3) "," (m 0) ")")}))))
+
+; Data ↦ Element
+(def bar1 (-> svg1 (.selectAll "g.bar")
+    (.data (clj->js data))
+  (.enter) (.append "g")
+    (.attr (clj->js {:class "bar"
+            :transform #(str "translate(0," (y1 %2) ")")}))))
+
+; Data Attributes ↦ Element Attributes
+(-> bar1 (.append "rect")
+    (.attr (clj->js {:width   #(x1 %)
+            :height  (.rangeBand y1)}))
+    (.style (clj->js {:fill   "indianred"
+             :stroke-opacity 0})))
+
+; Data Attributes ↦ Element Attributes
+(-> bar1 (.append "text")
+    (.attr (clj->js {:x  x1
+            :y  (/ (.rangeBand y1) 2)
+            :dx -6
+            :dy ".35em"
+            :text-anchor "end"}))
+    (.style "fill" "white")
+    (.text identity))
+
+;;;;;; a world with strokes!
+(strokes/bootstrap)
 
 ; x is a fn: data ↦ width
 (def x (-> d3 .-scale (.linear)
@@ -21,6 +66,38 @@
 (def y (-> d3 .-scale (.ordinal)
   (.domain (vec (range (count data))))
   (.rangeRoundBands [0 h] 0.2)))
+
+(def svg2 (-> d3 (.select "#static") (.append "svg")
+    (.attr {:width  (+ w (m 1) (m 3))
+            :height (+ h (m 0) (m 2))})
+  (.append "g")
+    (.attr {:transform (str "translate(" (m 3) "," (m 0) ")")})))
+
+; Data ↦ Element
+(def bar2 (-> svg2 (.selectAll "g.bar")
+    (.data data)
+  (.enter) (.append "g")
+    (.attr {:class "bar"
+            :transform #(str "translate(0," (y %2) ")")})))
+
+; Data Attributes ↦ Element Attributes
+(-> bar2 (.append "rect")
+    (.attr {:width   #(x %)
+            :height  (.rangeBand y)})
+    (.style {:fill   "indianred"
+             :stroke-opacity 0}))
+
+; Data Attributes ↦ Element Attributes
+(-> bar2 (.append "text")
+    (.attr {:x  x
+            :y  (/ (.rangeBand y) 2)
+            :dx -6
+            :dy ".35em"
+            :text-anchor "end"})
+    (.style "fill" "white")
+    (.text identity))
+
+;;;;;;;; now an animated version
 
 (def svg (-> d3 (.select "#animated") (.append "svg")
     (.attr {:width  (+ w (m 1) (m 3))
@@ -90,35 +167,3 @@
 
 ; initially click funciton is data-to-bar
 (cur-click-fn data-to-bar)
-
-;;;;;;;;;; let's make a version without animation...
-
-(def svg2 (-> d3 (.select "#static") (.append "svg")
-    (.attr {:width  (+ w (m 1) (m 3))
-            :height (+ h (m 0) (m 2))})
-  (.append "g")
-    (.attr {:transform (str "translate(" (m 3) "," (m 0) ")")})))
-
-; Data ↦ Element
-(def bar2 (-> svg2 (.selectAll "g.bar")
-    (.data data)
-  (.enter) (.append "g")
-    (.attr {:class "bar"
-            :transform #(str "translate(0," (y %2) ")")})))
-
-; Data Attributes ↦ Element Attributes
-(-> bar2 (.append "rect")
-    (.attr {:width   #(x %)
-            :height  (.rangeBand y)})
-    (.style {:fill   "indianred"
-             :stroke-opacity 0}))
-
-; Data Attributes ↦ Element Attributes
-(-> bar2 (.append "text")
-    (.attr {:x  x
-            :y  (/ (.rangeBand y) 2)
-            :dx -6
-            :dy ".35em"
-            :text-anchor "end"})
-    (.style "fill" "white")
-    (.text identity))
