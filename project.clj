96d77f897ff0caea4988bac4245d13d937c56b36
Tom White <tom@sixdozen.com>
2013-08-05 01:03:16 -0700

    roll strokes to 0.5.1-ss and d3 upgrade

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.drib/strokes "0.5.0"
+(defproject net.drib/strokes "0.5.1-SNAPSHOT"
   :description "strokes: d3 from clojurescript"
   :url "https://github.com/dribnet/strokes"
   :license {:name "Eclipse Public License"
