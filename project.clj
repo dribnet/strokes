b1a8febc3c520af58c2d2114506280c2e1c9122f
Tom White <tom@sixdozen.com>
2013-09-14 12:01:31 -0700

    incremented version to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.drib/strokes "0.5.1"
+(defproject net.drib/strokes "0.5.2-SNAPSHOT"
   :description "strokes: d3 from clojurescript"
   :url "https://github.com/dribnet/strokes"
   :license {:name "Eclipse Public License"
